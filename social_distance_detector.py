1cb681fe64376dbf2cb2da2d234ac356e13bc744
Saniyapathan <54326148+Saniyapathan@users.noreply.github.com>
2020-12-13 17:40:57 +0530

    Update social_distance_detector.py

--- social_distance_detector.py
+++ social_distance_detector.py
@@ -1,6 +1,5 @@
 # USAGE
-# python social_distance_detector.py --input pedestrians.mp4
-# python social_distance_detector.py --input pedestrians.mp4 --output output.avi
+
 
 # import the necessary packages
 from sdd import social_distancing_config as config
